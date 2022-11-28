e87b58a2aaf02a16987835b40b946f8abc526661
Ratmir Ish <ratnitro05@outlook.com>
2022-11-28 21:48:38 +0300

    add time service

---
+++ NetCoreSheduleTask/Services/TimeService.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+
+namespace NetCoreSheduleTask.Services
+{
+    public class TimeService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<TimeService> _logger;
+        private Timer? _timer = null;
+
+        public TimeService(ILogger<TimeService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Сервис погоды запущен");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(10));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            Get("https://localhost:7293/WeatherForecast");
+
+            _logger.LogInformation("Погода проверена");
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+
+        private string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
